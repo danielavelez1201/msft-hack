7f1e26cef061e41d2838a9145b8b409270ec9e54
Daniela Velez <danielavelez1201@gmail.com>
2020-06-28 01:51:58 -0400

    Back-end API calls

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -4,11 +4,14 @@ import {
   TouchableOpacity,
   Text,
   View,
-  Button,
+  Image,
+  FlatList
 } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Camera from './components/Camera';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 var CameraPage = require('./components/Camera');
 var imageArr = CameraPage.imageArr;
@@ -22,8 +25,13 @@ class App extends Component {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
+          options={{ title: '' }}
+          style = {page.container}
+        />
+        <Stack.Screen
+          name="PicturePage"
+          component={PicturePage}
           options={{ title: 'Take a picture!' }}
-
         />
         <Stack.Screen
           name="GamePage"
@@ -33,14 +41,40 @@ class App extends Component {
       </Stack.Navigator>
     </NavigationContainer>
       )
+
   }
 }
 
 function HomeScreen({ navigation }) {
   return (
+      <View style = {page.homepage}>
+        <Text style={page.title}>Findling</Text>
+
+      <View style={[{width: "50%", marginLeft:100, marginTop: 30}]}>
+        <Button
+        color="white"
+        title= "Start game setup!"
+        type="solid"
+        icon={
+          <Icon
+            name="arrow-right"
+            size={20}
+            color="white"
+          />
+          }
+        onPress={() =>
+          navigation.navigate('PicturePage', {name: 'PicturePage'})
+          }
+        />
+      </View>
+    </View>
+  );
+}
+
+function PicturePage({navigation}) {
+  return (
     <>
       <Camera />
-
       <Button
         title="Go to the game page"
         onPress={() =>
@@ -48,16 +82,54 @@ function HomeScreen({ navigation }) {
         }
       />
     </>
-
-  );
+  )
 }
+
 function GamePage({ route, navigation }) {
-
-  console.log("printing", imageArr);
   return (
-    <Text>{JSON.stringify(imageArr)}</Text>
-  );
+    <View style={page.container}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Dan'},
+            {key: 'Dominic'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={page.item}>{item.key}</Text>}
+        />
+      </View>
+  )
 }
 
+const page = StyleSheet.create({
+  homepage : {
+    backgroundColor: "#c2fffe",
+    flex: 1,
+  },
+  title : {
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "#000",
+    textAlign: 'center',
+    marginTop: 200
+
+  },
+  container: {
+    flex: 1,
+    padding: 24,
+    alignItems: "center"
+  },
+  text: {
+    color: "#000",
+    fontSize: 14,
+    fontWeight: "bold"
+  }
+})
 
 export default App;
--- mobileapp/components/Camera.js
+++ mobileapp/components/Camera.js
@@ -6,19 +6,28 @@ import {
   StyleSheet,
   Text,
   TouchableHighlight,
-  View
+  View,
+  Image
 } from 'react-native';
 
 var imageArr = [];
 exports.imageArr = imageArr;
+var objectArr = []
+exports.objectArr = objectArr;
+var transArr = []
+exports.transArr = transArr;
 
 export default class Camera extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      path: null,
+    };
   }
 
-
-  render() {
+  renderCamera() {
+    console.log("Camera");
     return (
       <RNCamera
         ref={cam => {
@@ -32,20 +41,75 @@ export default class Camera extends PureComponent {
     );
   }
 
+  renderImage() {
+
+    console.log("HI", this.state.path);
+     return (
+       <View>
+         <Image
+           source={{ uri: this.state.path }}
+           style={styles.preview}
+         />
+         <Text style={styles.capture} onPress={() => this.setState({ path: null })}>Retake</Text>
+       </View>
+
+     );
+   }
+
+   render() {
+     return (
+       <View style={styles.container}>
+         {this.state.path ? this.renderImage() : this.renderCamera()}
+       </View>
+     );
+   }
+
   takePicture = async () => {
     try {
       const data = await this.camera.takePictureAsync();
       this.setState({ path: data.uri });
+      console.log(this.state);
       // this.props.updateImage(data.uri);
-       console.log('Path to image: ' + data.uri);
+      console.log('Path to image: ' + data.uri);
 
-       imageArr.push(data.uri);
-       console.log(imageArr)
+      imageArr.push(data.uri);
+      var objectArr = ['apple', 'tomato', 'sofa']
+      var obj_list = [];
+      for (i=0; i < objectArr.length; i++) {
+        obj_list.push({"Text": objectArr[i]})
+      }
+      await fetch("https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=es", {
+        method: "POST",
+        headers: {
+          "Ocp-Apim-Subscription-Key": "154d9fe476724f5384eb2e16efebc6ba",
+          "Content-Type": "application/json; charset=UTF-8",
+          "Ocp-Apim-Subscription-Region": "australiaeast"
+        },
+        body: JSON.stringify(obj_list)
+      }).then(response => response.json())
+      .then(json => {
+        transArr.push(json[0].translations);
+      });
+      console.log(transArr)
+      var obj_trans = transArr[0][0]["text"];
+      var obj = "apple";
+      fetch("https://flask-mongodb-app.azurewebsites.net/create?obj=" + obj + "&&trans=" + obj_trans);
+
+      fetch("https://flask-mongodb-app.azurewebsites.net/see", {
+        method: "GET"
+      }).then(response => response.json())
+      .then(json => {
+        console.log(json);
+      });
+
     } catch (err) {
       console.log('err: ', err);
     }
   };
 }
+
+
+
 
 
 
@@ -60,7 +124,9 @@ const styles = StyleSheet.create({
   preview: {
     flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center'
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
   },
   capture: {
     flex: 0,
